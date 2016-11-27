cbea08673a4c5f394c7bd0da683cea3eba4e5f8a
mickish <mickish@ndr.com>
2016-11-27 08:30:19 -0500

    Load event data with $http service

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,44 +1,19 @@
-eventsApp.factory('eventData', function() {
+eventsApp.factory('eventData', function($http, $q) {
     return {
-        event: {
-            name: 'Angular Boot Camp',
-            date: 1359781015626,
-            date2: new Date(1359781015626),
-            //date: '1/1/2013',
-            time: '10:30 am',
-            price: 34,
-            location: {
-                address: 'Google Headquarters',
-                city: 'Mountain View',
-                province: 'CA'
-            },
-            imageUrl: '/img/AngularJS_logo.svg.png',
-            sessions: [
-                {
-                    name: 'Directives Masterclass',
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives.',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Scopes for fun and profit',
-                    creatorName: 'John Doe',
-                    duration: 2,
-                    level: 'Introductory',
-                    abstract: 'This session will take a closer look at scopes.',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Well Behaved Controllers',
-                    creatorName: 'Jane Doe',
-                    duration: 4,
-                    level: 'Intermediate',
-                    abstract: 'Controllers are the beginning of everything Angular does.',
-                    upVoteCount: 0
-                }
-            ]
+
+        /* returns a promise */
+        getEvent: function() {
+            var deferred = $q.defer();
+
+            $http({method:'GET', url:'/data/event/1.json'})
+                .success( function (data, status, headers, config) {
+                    deferred.resolve(data);
+                })
+                .error( function (data, status, headers, config) {
+                    deferred.reject(status);
+                });
+
+            return deferred.promise;
         }
-    };
+    }
 });
