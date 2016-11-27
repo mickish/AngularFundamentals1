04c989caf99e5b7239c298c15a15204bc0ef58f7
mickish <mickish@ndr.com>
2016-11-27 15:30:31 -0500

    Added $cacheFactory service

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Event registration application
 
 Based on [Pluralsight] AngularJS Fundamentals
 by Joe Eames and Jim Cooper
+
+Customized web-server.js to handle $resource.save() thanks to:
+ http://stackoverflow.com/questions/19618010/resource-save-is-not-functioning#20283629
+ https://github.com/glepretre/angular-seed
--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var eventsApp = angular.module('eventsApp', ['ngSanitize','ngResource']);
+var eventsApp = angular.module('eventsApp', ['ngSanitize','ngResource'])
+    .factory('myCache', function($cacheFactory) {
+        return $cacheFactory('myCache', {capacity:3})
+    });
